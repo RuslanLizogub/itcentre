6362f58db6ae2f2cbf5508419d94607c0478ef4a
ruslan_lizogub <ruslan.lizogub0201@gmail.com>
2015-04-06 16:29:12 +0300

    level12

--- src/HomeWork/Level1_1.java
+++ src/HomeWork/Level1_1.java
@@ -3,6 +3,7 @@ package HomeWork;
 public class Level1_1 {
 	public static void main(String args[ ]) { 
 		System.out.println( "Welcome to the world of Java" ); 
-		System.out.println( "Welcome toooooooooooooooo  gggit!!!!" ); 
+		System.out.println( "Welcome toooooooooooooooo  gggit!!!!" );
+		System.out.println( "Welcome toooooooooooooooo git 3333!!!!" );
     } 
 }
