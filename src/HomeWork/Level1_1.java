a947320552db8fa42da277844cc08899ea75f607
ruslan_lizogub <ruslanlizogub0201@gmail.com>
2015-04-06 16:20:18 +0300

    test commit

--- src/HomeWork/Level1_1.java
+++ src/HomeWork/Level1_1.java
@@ -3,5 +3,6 @@ package HomeWork;
 public class Level1_1 {
 	public static void main(String args[ ]) { 
 		System.out.println( "Welcome to the world of Java" ); 
+		System.out.println( "Welcome toooooooooooooooo  gggit!!!!" ); 
     } 
 }
